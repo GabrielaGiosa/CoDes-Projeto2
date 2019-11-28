5febd76d6aae1955168453267651a22b525beb16
Daniel Minson Pucciariello <49560569+danielminson@users.noreply.github.com>
2019-11-28 20:01:16 -0300

    fazendo varias coisas

--- assets/js/pedidos.js
+++ assets/js/pedidos.js
@@ -1,1 +1,15 @@
+var mesa;
+var pagina;
+var mesa atual;
+
+let seleciona = function(element){
+	name = element.classList[0]
+	window.localStorage.setItem('mesa', name[name.length - 1])
+};
+
+
 document.addEventListener('DOMContentLoaded', function() {
+	window.localStorage.getItem('pagina')
+	
+	window.localStorage.setItem('pagina', 'pedidos')
+})
---
+++ assets/js/pedidos_cadeira.js
@@ -0,0 +1,7 @@
+let seleciona = function(element){
+	window.localStorage.getItem('mesa')
+};
+
+
+document.addEventListener('DOMContentLoaded', function() {
+})
