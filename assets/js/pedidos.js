eac27a343b43274ee62b0479d80f35aa9d6d271c
Daniel Minson Pucciariello <49560569+danielminson@users.noreply.github.com>
2019-11-28 22:00:40 -0300

    .

--- assets/js/pedidos.js
+++ assets/js/pedidos.js
@@ -1,6 +1,5 @@
 var mesa;
 var pagina;
-var mesa atual;
 
 let seleciona = function(element){
 	name = element.classList[0]
@@ -9,7 +8,37 @@ let seleciona = function(element){
 
 
 document.addEventListener('DOMContentLoaded', function() {
-	window.localStorage.getItem('pagina')
-	
+
+ 	var mesa_atual= window.localStorage.getItem('mesa')
+	console.log(mesa_atual)	
+
+ 	if (mesa_atual ==1) {
+ 		document.getElementsByClassName("mesa_1")[0].classList.add("ativo");
+    }
+ 	if (mesa_atual ==2) {
+ 		document.getElementsByClassName("mesa_2")[0].classList.add("ativo");
+    }
+ 	if (mesa_atual ==3) {
+ 		document.getElementsByClassName("mesa_3")[0].classList.add("ativo");
+    }
+ 	if (mesa_atual ==4) {
+ 		document.getElementsByClassName("mesa_4")[0].classList.add("ativo");
+    }
+ 	if (mesa_atual ==5) {
+ 		document.getElementsByClassName("mesa_5")[0].classList.add("ativo");
+    }
+ 	if (mesa_atual ==6) {
+ 		document.getElementsByClassName("mesa_6")[0].classList.add("ativo");
+    }
+ 	if (mesa_atual ==7) {
+ 		document.getElementsByClassName("mesa_7")[0].classList.add("ativo");
+    }
+ 	if (mesa_atual ==8) {
+ 		document.getElementsByClassName("mesa_8")[0].classList.add("ativo");
+    }
+
 	window.localStorage.setItem('pagina', 'pedidos')
+	// if (mesa !=1) {
+	// 	document.getElementsByClassName("mesa_1")[0].classList.remove("ativo");
+ //    }
 })
